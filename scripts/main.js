aa8ee2f8d94ebbe388efd57703434d010e41cac6
beeant <pdelatour7@gmail.com>
2018-09-26 15:47:34 +0200

    Ajout de scripts/main.js

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+var name = prompt ("Hi! What's your name? :");
+alert=("Nice to see you here " + name + "!");
+
+var video = prompt ("Hey le site xxxvidsxxx est trop bien. Viens dessus "+ name+" stp please")
