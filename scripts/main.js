27cf3ff64b4cb03b2e52d5bb94d4ec370bfd8020
beeant <pdelatour7@gmail.com>
2018-09-28 19:31:14 +0200

    storage de bouts de code

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,3 @@
-var name = prompt ("Hi! What's your name? :");
-alert=("Nice to see you here " + name + "!");
-
-var video = prompt ("Hey le site xxxvidsxxx est trop bien. Viens dessus "+ name+" stp please")
+function popupBasique() {
+  window.open(document.getElementById("popup"));
+}
